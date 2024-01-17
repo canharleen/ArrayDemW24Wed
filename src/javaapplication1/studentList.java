d48b718af587b03e4316ceddfd42f6bcad400401
Harleen Kaur <kau15579@sheridancollege.ca>
2024-01-17 09:59:20 -0500

    Third commit

--- src/javaapplication1/studentList.java
+++ src/javaapplication1/studentList.java
@@ -13,6 +13,7 @@ public class studentList {
         Student s1 = new Student(1,"Harleen");
         Student[] studentList = new Student[4];
         studentList[0]=s1;
+        System.out.println("Third commit");
         studentList[1] = new Student(2,"Pawan");
         studentList[2] = new Student(3,"Pawanjot");
         for (int i=0; i<studentList.length;i++){
