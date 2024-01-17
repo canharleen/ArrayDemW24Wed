44df5b2ffa382376c9dea13119099cec40e3336a
Harleen Kaur <kau15579@sheridancollege.ca>
2024-01-17 09:45:05 -0500

    First commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ * Date: Jan 10, 2024
+ * This is my first program using netbeans
+ * @author gilla
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Enter a word");
+        Scanner c = new Scanner(System.in);
+        String word = c.nextLine();
+        System.out.println(word);
+        
+        char[] myLetters = new char[word.length()];
+        for (int i=0;i<myLetters.length;i++){
+            
+            myLetters[i]=word.charAt(i);
+        }
+        
+        for (int i=myLetters.length;i>0;i--){
+            System.out.println(myLetters[i-1]);
+        }
+        
+    }
+    
+}
