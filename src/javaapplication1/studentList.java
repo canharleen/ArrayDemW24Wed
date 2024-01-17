3868db90df4adb37be84ceb74cd227d77e5d7cdc
Harleen Kaur <kau15579@sheridancollege.ca>
2024-01-17 10:03:29 -0500

    Reverting third commit

--- src/javaapplication1/studentList.java
+++ src/javaapplication1/studentList.java
@@ -13,7 +13,6 @@ public class studentList {
         Student s1 = new Student(1,"Harleen");
         Student[] studentList = new Student[3];
         studentList[0]=s1;
-        System.out.println("Third commit");
         studentList[1] = new Student(2,"Pawan");
         studentList[2] = new Student(3,"Pawanjot");
         for (int i=0; i<studentList.length;i++){
